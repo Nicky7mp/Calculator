43fadf4291a40c10117291bf158317f69a8573c7
codenation <codenation@codenations-MacBook-Pro.local>
2019-08-13 15:22:36 +0100

    Updates

---
+++ precache-manifest.4efca1b372dbec842c3dd8a65ad29aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "776d55bbc9314a254a225779bce143c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc604800a678793d45d1",
+    "url": "/static/css/main.01a07f8f.chunk.css"
+  },
+  {
+    "revision": "a96593a07d142e264f4f",
+    "url": "/static/js/2.74f9e195.chunk.js"
+  },
+  {
+    "revision": "fc604800a678793d45d1",
+    "url": "/static/js/main.1c798abb.chunk.js"
+  },
+  {
+    "revision": "b6d43c60bae782112689",
+    "url": "/static/js/runtime~main.48e4b13a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
